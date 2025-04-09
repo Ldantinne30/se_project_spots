16c265ad06396a8fc713f5460ee20ff4b84e2bbc
Ldantinne30 <lucasdantinne30@gmail.com>
2025-04-08 20:28:25 -0500

    opening and closing modals

--- scripts/index.js
+++ scripts/index.js
@@ -27,7 +27,26 @@ const initialCards = [
 
 console.log(initialCards);
 
-const editProfileButton = document.querySelector(".profile__edit-btn");
-
+const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+
+const newPostBtn = document.querySelector(".profile__post-btn");
+const newPostModal = document.querySelector("#new-post-modal");
+const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
+
+editProfileBtn.addEventListener("click", function () {
+  editProfileModal.classList.add("modal__opened");
+});
+
+editProfileCloseBtn.addEventListener("click", function () {
+  editProfileModal.classList.remove("modal__opened");
+});
+
+newPostBtn.addEventListener("click", function () {
+  newPostModal.classList.add("modal__opened");
+});
+
+newPostCloseBtn.addEventListener("click", function () {
+  newPostModal.classList.remove("modal__opened");
+});
